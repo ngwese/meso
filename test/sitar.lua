b16912290257c31c9f113d02ed0439222a510f17
Greg Wuller <greg@afofo.com>
2020-02-16 22:20:31 -0800

    sitar: begin mpe experiment

--- test/sitar.lua
+++ test/sitar.lua
@@ -1,6 +1,8 @@
 local mu = require('musicutil')
 local hs = require('awake/lib/halfsecond')
-SitarEngine = require('meso/lib/sitar_engine')
+local SitarEngine = require('meso/lib/sitar_engine')
+local sky = include('meso/lib/sky/process')
+local mpe = include('meso/lib/sky/mpe')
 
 engine.name = 'Sitar'
 
@@ -8,9 +10,42 @@ tuning_base = mu.note_num_to_freq(48)
 chikari = {1, 16/15, 5/4, 4/3, 3/2, 8/5, 15/8, 2}
 tarafdar = {1, 16/15, 5/4, 4/3, 3/2, 8/5, 15/8, 2, 2*16/15, 2*5/4, 2*4/3, 2*3/2}
 
+
+function render(event, output, state)
+--  local x = util.linlin(0, 127, 0, 64, event.num)
+  local x = event.note
+  local y = util.linlin(0, 127, 64, 0, event.cc74)
+  local r = util.linlin(0, 127, 2, 15, event.pressure)
+  screen.clear()
+  --screen.move(10, 10)
+  --screen.text(event.pressure)
+  screen.circle(x, y, r)
+  screen.close()
+  screen.stroke()
+  screen.update()
+end
+
+chain = sky.Chain{
+  -- output events as midi
+  --mpe.Process{},
+  sky.Logger{},
+  --sky.Func(render)
+}
+
+local source = sky.Input{
+  name = "ContinuuMini",
+  chain = chain,
+}
+
+local source2 = sky.Input{
+  name = "Lightpad BLOCK",
+  chain = chain,
+}
+
+
 function key(n, z)
   if n == 2 and z == 1 then
-    local which = math.random(#chikari) - 1
+    local which = math.random(#chikari - 1)
     local amp = (math.random(25) + 5) / 100
     engine.pluck(which, amp)
   end
@@ -20,6 +55,11 @@ function key(n, z)
   end
 end
 
+function redraw()
+  screen.clear()
+  screen.update()
+end
+
 function init()
   hs.init()
   params:set('delay', 0.13)
@@ -27,4 +67,8 @@ function init()
   params:set('delay_feedback', 0.27)
   SitarEngine.init_tuning(chikari, tarafdar, tuning_base)
   SitarEngine.add_params()
+end
+
+function cleanup()
+  source:cleanup()
 end
