9ddf7032850d357f281b50c5f984840964467e57
Greg Wuller <greg@afofo.com>
2020-06-17 10:17:47 -0700

    progress, need hid-write norns branch, not working

--- lib/powermate.lua
+++ lib/powermate.lua
@@ -6,28 +6,61 @@
 
 local Powermate = {}
 Powermate.__index = Powermate
+Powermate.MAGIC_SPEED_SHIFT = 8
+Powermate.MAGIC_PULSE_SHIFT = 17
+Powermate.MAGIC_ASLEEP_SHIFT = 19
+Powermate.MAGIC_AWAKE_SHIFT = 20
 
-Powermate.connect = function(num)
-  local o = setmetatable({}, Powermate)
+function Powermate.connect(num)
+  local self = setmetatable({}, Powermate)
 
   -- no-op handlers
-  o.enc = function(num, delta)
+  self.enc = function(num, delta)
   end
   
-  o.key = function(num, z)
+  self.key = function(num, z)
   end
   
   -- connect up device
-  o.device = hid.connect(num)
-  o.device.event = function(type, code, value)
+  self.device = hid.connect(num)
+  self.device.event = function(type, code, value)
     if type == 2 and code == 7 then
-      o.enc(1, value)
+      self.enc(1, value)
     elseif type == 1 and code == 256 then
-      o.key(1, value)
+      self.key(1, value)
     end
   end
 
-  return o
+  return self
+end
+
+function Powermate:_set_cfg(brightness, pulse_speed, asleep, awake, pulse_table)
+  pulse_table = pulse_table or 0
+  
+  if asleep or false then asleep = 1 else asleep = 0  end
+  if awake or false then awake = 1 else awake = 0 end
+  
+  local value = (
+    brightness  |
+    pulse_speed << self.MAGIC_SPEED_SHIFT  |
+    pulse_table << self.MAGIC_PULSE_SHIFT  |
+    asleep      << self.MAGIC_ASLEEP_SHIFT |
+    awake       << self.MAGIC_AWAKE_SHIFT
+  )
+  
+  print(value)
+  local etype = self.device.device.types.EV_MSC   -- yuck
+  local code = self.device.device.codes.MSC_PULSELED
+  return self.device.device:send(etype, code, value)
+end
+
+function Powermate:set_steady_led(brightness)
+  self:_set_cfg(brightness, 0, false, false)
+end
+
+function Powermate:set_pulse(speed)
+  -- pulse speed (0-510)
+  self:_set_cfg(0, speed, false, false)
 end
 
 return Powermate
